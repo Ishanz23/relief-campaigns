7454fca86297434ca7c0e42acd556a2f963a68eb
Sayantan Dey <sayantandey23@gmail.com>
2021-05-30 22:24:27 +0530

    fix: desktop layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,10 +5,10 @@ import Layout from '../components/Layout'
 const IndexPage = () => {
   return (
     <Layout header={true}>
-      <header className='w-full h-16 flex items-center justify-center md:justify-start md:px-8'>
+      <header className='w-full h-16 flex items-center justify-center md:px-8'>
         <h1 className='sm:text-2xl md:text-4xl font-bold uppercase'>Upcoming Relief Campaigns</h1>
       </header>
-      <section className='h-full w-full p-2 md:p-8 md:grid md:grid-cols-1 gap-4'>
+      <section className='h-full w-1/2 mx-auto p-2 md:p-8 md:grid md:grid-cols-1 gap-4'>
         <MousuniCard name={'mousuni'}></MousuniCard>
       </section>
     </Layout>
