8f7a097a6e3a9afaf1872736bc446dc106a994f6
Sayantan Dey <sayantandey23@gmail.com>
2021-05-30 22:28:31 +0530

    fix: home mobile layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ const IndexPage = () => {
       <header className='w-full h-16 flex items-center justify-center md:px-8'>
         <h1 className='sm:text-2xl md:text-4xl font-bold uppercase'>Upcoming Relief Campaigns</h1>
       </header>
-      <section className='h-full w-1/2 mx-auto p-2 md:p-8 md:grid md:grid-cols-1 gap-4'>
+      <section className='h-full md:w-1/2 md:mx-auto p-2 md:p-8 md:grid md:grid-cols-1 gap-4'>
         <MousuniCard name={'mousuni'}></MousuniCard>
       </section>
     </Layout>
