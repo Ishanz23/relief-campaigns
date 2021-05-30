9a02d9e510a910e922fb41cffd199bae608f013b
Sayantan Dey <sayantandey23@gmail.com>
2021-05-30 22:11:18 +0530

    fix: home routes

--- src/components/MousuniCard.tsx
+++ src/components/MousuniCard.tsx
@@ -3,12 +3,12 @@ import { StaticImage } from 'gatsby-plugin-image'
 import { Link } from 'gatsby'
 
 // markup
-export default function MousuniCard() {
+export default function MousuniCard({ name }) {
   return (
-    <div className='w-full flex flex-col my-8 md:my-0 backdrop-filter bg-gray-600 bg-opacity-40 backdrop-blur-md shadow-lg rounded-md'>
+    <div className='w-full flex flex-col my-4 md:my-0 backdrop-filter bg-gray-600 bg-opacity-40 backdrop-blur-md shadow-lg rounded-md'>
       <header className='w-full flex items-center justify-between p-4'>
-        <Link to='mousuni'>
-          <h1 className='text-xl text-center uppercase font-bold'>Save Mousuni</h1>
+        <Link to={`/${name}`}>
+          <h1 className='text-xl text-center uppercase font-bold'>Save {name}</h1>
         </Link>
         <a href='https://goo.gl/maps/jhFoS9c1h8K29dbM9' target='_'>
           <svg xmlns='http://www.w3.org/2000/svg' className='h-6 w-6' viewBox='0 0 20 20' fill='currentColor'>
@@ -20,27 +20,29 @@ export default function MousuniCard() {
           </svg>
         </a>
       </header>
-      <div>
-        <StaticImage src='https://i.imgur.com/iG8XDyi.jpeg' alt='Save Mousuni'></StaticImage>
-      </div>
-      <div className='p-4'>
-        <p className=''>
-          Mousuni Islands is a collection of small islands consisting around 17 villages, located near Sagaddwip and
-          Bakkhali.
-        </p>
-        <p className=''>
-          Due to the recent cyclone 'Yaas', combined with the effect of full moon on the same day, most of the villages
-          suffered massive damages due to severe flooding.
-        </p>
-      </div>
+      <Link to={`/${name}`}>
+        <div>
+          <StaticImage src='https://i.imgur.com/iG8XDyi.jpeg' alt='Save Mousuni'></StaticImage>
+        </div>
+        <div className='p-4'>
+          <p className=''>
+            Mousuni Islands is a collection of small islands consisting around 17 villages, located near Sagaddwip and
+            Bakkhali.
+          </p>
+          <p className=''>
+            Due to the recent cyclone 'Yaas', combined with the effect of full moon on the same day, most of the
+            villages suffered massive damages due to severe flooding.
+          </p>
+        </div>
+      </Link>
       <div className='actions flex items-center justify-between'>
         <Link
-          to='/mousuni'
+          to={`/${name}`}
           className='flex-1 p-4 font-bold bg-gray-600 text-center backdrop-filter bg-opacity-40 backdrop-blur-md md:rounded-bl-md'>
           DETAILS
         </Link>
         <Link
-          to='/mousuni/contribute'
+          to={`/${name}/contribute`}
           className='flex-1 p-4 font-bold bg-gray-500 text-center backdrop-filter bg-opacity-40 backdrop-blur-md md:rounded-br-md'>
           CONTRIBUTE
         </Link>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,10 +6,10 @@ const IndexPage = () => {
   return (
     <Layout header={true}>
       <header className='w-full h-16 flex items-center justify-center md:justify-start md:px-8'>
-        <h1 className='text-2xl md:text-4xl font-bold uppercase'>Upcoming Relief Campaigns</h1>
+        <h1 className='sm:text-2xl md:text-4xl font-bold uppercase'>Upcoming Relief Campaigns</h1>
       </header>
       <section className='h-full w-full p-2 md:p-8 md:grid md:grid-cols-1 gap-4'>
-        <MousuniCard></MousuniCard>
+        <MousuniCard name={'mousuni'}></MousuniCard>
       </section>
     </Layout>
   )
